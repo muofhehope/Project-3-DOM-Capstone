a4e0955ae7105e9ec178b00e32627ce463968a38
mehTro20 <justmondlisok@gmail.com>
2025-05-22 10:00:32 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+  document.getElementById("btn1").onclick = () => openModal("modal1");
+  document.getElementById("btn2").onclick = () => openModal("modal2");
+  document.getElementById("btn3").onclick = () => openModal("modal3");
+
+  function openModal(id) {
+    document.getElementById(id).style.display = "block";
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+  }
+
+  // Close modal if clicked outside
+  window.onclick = function(event) {
+    ["modal1", "modal2", "modal3"].forEach(id => {
+      const modal = document.getElementById(id);
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  }
+  
